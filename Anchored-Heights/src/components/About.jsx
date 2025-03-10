ec2c98813993ba27b9db8437ea16c8081382ffea
Leon Munene Njeru <106140292+Leon8M@users.noreply.github.com>
2025-03-10 16:56:45 +0000

    txt

--- Anchored-Heights/src/components/About.jsx
+++ Anchored-Heights/src/components/About.jsx
@@ -22,14 +22,14 @@ const About = () => {
         {/* About Content */}
         <div className="space-y-6">
           <p className="text-lg text-gray-700 leading-relaxed">
-            At <span className="font-semibold text-[#1D3557]">Vertical Techniques</span>, we specialize in high attitude abseil window cleaning and replacement, retrieval plastering minor painting, leak repairs, silicone applications, solar panel cleaning, Alucobond repair, replacement and installation of equipment and rope access anchors.
+            At <span className="font-semibold text-[#1D3557]">Vertical Techniques</span>, we specialize in high attitude Abseil window cleaning and replacement, retrieval, sealing cracks & plastering, minor painting, leak repairs, silicone applications, solar panel cleaning, Alucobond repair, replacement and installation of equipment and rope access anchors.
             Our team of highly skilled and competent technicians delivers exceptional service with a focus on efficiency and safety, saving you both time and resources.
           </p>
           <p className="text-lg text-gray-700 leading-relaxed">
             Our experienced rope access professionals excel in completing high-rise short-term tasks and complex high-altitude projects on time. We eliminate the need for cumbersome scaffolding setups and breakdowns, ensuring your projects are finished quickly and without any interruptions to your core operations.
           </p>
           <p className="text-lg text-gray-700 leading-relaxed">
-            Discover how <span className="font-semibold text-[#1D3557]">Vertical Techniques</span> can elevate your cleaning experience with our rope access expertise. Whether you need Façade cleaning, light installation, structural repairs or exterior equipment setup, we deliver outstanding result sufficiently minimizing disruption to optimize your time and resources.
+            Discover how <span className="font-semibold text-[#1D3557]">Vertical Techniques</span> can elevate your building’s aesthetics with our rope access expertise. Whether you need Façade cleaning, light installation, structural repairs or exterior equipment setup, we deliver outstanding result sufficiently minimizing disruption to optimize your time and resources.
           </p>
         </div>
 
