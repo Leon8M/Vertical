c734bd6cc4be0a5e3486b505da08e3c3fbfaab5e
Leon Munene <leonmunene@gmail.com>
2025-04-05 14:25:41 +0300

    img err fix

--- Anchored-Heights/vite.config.js
+++ Anchored-Heights/vite.config.js
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Anchored-Heights/', 
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/*',
+          dest: ''
+        }
+      ]
+    })
+  ],
+  base: '/Anchored-Heights/',
   build: {
-    outDir: '../dist',     
-    emptyOutDir: true,         
-    sourcemap: false          
+    outDir: '../dist',
+    emptyOutDir: true,
   }
 });
